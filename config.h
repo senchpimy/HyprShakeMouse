570a6ebc52cdb76e0a7f52802dc9d94716b5650f
senchpimy <senchpimy@gmail.com>
2025-04-05 17:01:18 -0600

    config file

---
+++ config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Configuración de sensibilidad del cursor
+#define SENSIBILITY         10    // Entre más alto, menos sensible
+#define DISTANCE_SENSIBILITY 250  // Entre más alto, menos sensible
+#define TIME_TO_REVERT      80    // Tiempo para revertir tamaño del cursor (ms)
+
+// Configuración del dock
+#define DOCK_HEIGHT         100   // Altura del dock en píxeles
+
+#endif // CONFIG_H
