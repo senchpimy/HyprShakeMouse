de0a0f04acaa2cf1141f1631ce5b9f205a482b8f
senchpimy <senchpimy@gmail.com>
2025-05-19 13:20:12 -0600

    Tablet mode files

---
+++ tabletmode.cpp
@@ -0,0 +1,29 @@
+#include <array>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+std::string exec(const char *cmd) {
+  std::array<char, 128> buffer;
+  std::string result;
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  if (!pipe) {
+    throw std::runtime_error("popen() failed!");
+  }
+  while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) !=
+         nullptr) {
+    result += buffer.data();
+  }
+  return result;
+}
+
+void setTabletMode() {
+  std::string command = "ags run-js 'tabletMode();'";
+  try {
+    exec(command.c_str());
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+}
---
+++ tabletmode.h
@@ -0,0 +1,6 @@
+#ifndef TABLETMODE_H
+#define TABLETMODE_H
+
+void setTabletMode();
+
+#endif
